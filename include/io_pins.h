44f7e32e174d4374d4d2c9da797a952f0273b212
CK1 <kreissc1@gmail.com>
2022-03-16 11:13:23 +0100

    Initial Setup für Arduino Pro Mini

--- include/io_pins.h
+++ include/io_pins.h
@@ -1,9 +1,9 @@
-#define PIN_DHT 3
-#define PIN_INFO_LED 9
+#define PIN_DHT 7 //Not used in my application
+#define PIN_INFO_LED A1 //Not used in my application
 
 // PIN definition for module
 #define PIN_LMIC_NSS  10
-#define PIN_LMIC_RST  A0
-#define PIN_LMIC_DIO0 5
-#define PIN_LMIC_DIO1 6
+#define PIN_LMIC_RST  9
+#define PIN_LMIC_DIO0 2
+#define PIN_LMIC_DIO1 8
 #define PIN_LMIC_DIO2 7
