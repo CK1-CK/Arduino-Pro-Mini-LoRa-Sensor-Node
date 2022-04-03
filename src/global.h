f4cff8637439e1cbb2de4ef5ed2fbf91d97dc923
CK1 <kreissc1@gmail.com>
2022-04-03 19:20:44 +0200

    English variables names

--- src/global.h
+++ src/global.h
@@ -7,6 +7,6 @@
 extern volatile int POWER_DOWN_SLEEP_COUNTER;
 extern volatile uint8_t door_state; // DOOR-Status open/closed
 extern volatile uint8_t watchdog; // Watchdog, Differentiation between watchdog and real alarm.
-extern volatile unsigned long alteZeit, entprellZeit; //Debouncing/Entprellung Switch
+extern volatile unsigned long oldTime, debounceTime; //Debouncing/Entprellung Switch
 
 #endif
