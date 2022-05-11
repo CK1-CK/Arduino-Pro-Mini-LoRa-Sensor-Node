cc5e27489aa7aa1f5405b5c6a835c74fc2357a09
CK1 <kreissc1@gmail.com>
2022-05-11 12:01:09 +0200

    New Interrupt Processing

--- src/main.cpp
+++ src/main.cpp
@@ -6,38 +6,47 @@
 #include "powerdown.h"
 #include "version.h"
 #include "version_build.h"
+#include "settings.h"
 
 void interruptDoorFunction() // Interrupt Function for DoorSwitch
 {
-
   if ((millis() - oldTime) > debounceTime) // Debouncing/Entprellung Switch
   {
     door_state = digitalRead(PIN_DOOR_SWITCH);
     watchdog = 0;       // Real Alarm  // Watchdog/Heartbeat, Differentiation between watchdog and real alarm.   1=WatchDogSignal 0=Real DoorAlarm
     oldTime = millis(); // Remember last run time.
-    
+
     // Debug
-    // Serial.print("Interrupt Routine: DoorState: ");
-    // Serial.println(door_state, DEC);
+    Serial.print("Interrupt Routine: DoorState: ");
+    Serial.println(door_state, DEC);
   }
 }
 
 void CheckDoorStateAndSendLoraPackage()
 {
-  if (watchdog == 0)
+  if (watchdog == 0 && door_state == 0)
   {
+    detachInterrupt(digitalPinToInterrupt(PIN_DOOR_SWITCH)); // Disable Interrupt Function for DoorSwitch
+    Serial.println("Interrupt Routine disabled.");
+
     disableDeepSleep();
-    
-    // Send Lora Package
+    setTX_Interval(10); // Send Lora Package during the next n seconds
+
+    // Queue Lora Package
     LoRaWANDo_send(&sendjob);
+
+    // Debug
+    Serial.print("DoorState: ");
+    Serial.println(door_state, DEC);
   }
+
   watchdog = 1; // Reset Watchdog
 }
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println(F("Starting DHT ..."));
+  Serial.println("Starting...");
   Serial.println("Sketch: " VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH "." BUILD_COMMIT "-" BUILD_BRANCH);
   Serial.println("Builddate: " BUILD_DATE " " BUILD_TIME);
   PrintResetReason();
