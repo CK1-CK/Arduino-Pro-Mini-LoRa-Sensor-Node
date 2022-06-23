a130425a8b5f7518b383297c7959c6522fe89bf3
CK1 <kreissc1@gmail.com>
2022-06-23 09:45:52 +0200

    Debounce Time increased --> 1000ms

--- src/global.cpp
+++ src/global.cpp
@@ -3,4 +3,4 @@
 
 volatile uint8_t door_state = 0; // DOOR-Status open/closed
 volatile uint8_t watchdog = 1; // Watchdog, Differentiation between watchdog and real alarm.   1=WatchDogSignal 0=Real DoorAlarm
-volatile unsigned long oldTime=0, debounceTime=100; //Debouncing/Entprellung Switch
+volatile unsigned long oldTime=0, debounceTime=1000; //Debouncing/Entprellung Switch
