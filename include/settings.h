ef59a3085088fdaab88205c7b6ed914849f18f25
CK1 <kreissc1@gmail.com>
2022-08-13 19:36:01 +0200

    Increase Send Intervall for KeepAlive/Watchdog

--- include/settings.h
+++ include/settings.h
@@ -1,2 +1,2 @@
-#define LORA_TX_INTERVAL 900 //KeepAlive-WatchDog Signal every n Secondes
+#define LORA_TX_INTERVAL 1200 //KeepAlive-WatchDog Signal every n Secondes
 #define LMIC_LORA_SF  DR_SF7 // LORA Data rate
