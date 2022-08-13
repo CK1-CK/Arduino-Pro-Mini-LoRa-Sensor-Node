ace137d85ad330bbcc2d6205935a28a495b42ecb
CK1 <kreissc1@gmail.com>
2022-08-13 17:54:45 +0200

    AlarmMode Selector

--- src/global.cpp
+++ src/global.cpp
@@ -3,4 +3,5 @@
 
 volatile uint8_t door_state = 0; // DOOR-Status open/closed
 volatile uint8_t watchdog = 1; // Watchdog, Differentiation between watchdog and real alarm.   1=WatchDogSignal 0=Real DoorAlarm
+volatile uint8_t AlarmMode_Enabled = 0; // Alarmmode 1=Enabled
 volatile unsigned long oldTime=0, minSendIntervall=300000;
