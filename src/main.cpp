892e3a663e22d961fae23b2d7d24262b8e3bdfd4
CK1 <kreissc1@gmail.com>
2022-08-13 17:58:04 +0200

    Resetfunction added, SwitchCheck polled, NoISR

--- src/main.cpp
+++ src/main.cpp
@@ -8,28 +8,27 @@
 #include "version_build.h"
 #include "settings.h"
 
-unsigned int test = 0;
 long door_counter = -1;
 
-void CheckDoorStateAndSendLoraPackage()
+void resetToDefaultValues()
 {
-  Serial.println("CheckDoorStateAndSendLoraPackage");
-  if (millis() - oldTime > minSendIntervall) // Debouncing/Entprellung Switch - The millis() function will overflow (go back to zero), after approximately 50 days. (Max value = 4.294.967.295)
+  watchdog = 1;                              // Reset Watchdog
+  door_state = digitalRead(PIN_DOOR_SWITCH); // Reset Doorstate
+
+  // Serial.println("resetToDefaultValues");
+}
+
+void CheckAlarm_SendAlarmLoraPackage()
+{
+  if ((millis() - oldTime > minSendIntervall) && AlarmMode_Enabled) // Debouncing/Entprellung Switch - The millis() function will overflow (go back to zero), after approximately 50 days. (Max value = 4.294.967.295)
   {
     if (watchdog == 0) // Real Alarm?
     {
       // Queue Lora Package
-      LoRaWANDo_send(&sendjob);
+      LoRaWANDo_send(&sendjob); // Send Alarm Message
 
       // Debug
-      // Serial.print("DoorState: ");
-      // Serial.println(door_state, DEC);
-
-      watchdog = 1; // Reset Watchdog
-
-      // Debug
-      Serial.println("Scheisse!!");
-      Serial.println(test++, DEC);
+      Serial.println("Alarm Package queued!!");
     }
     oldTime = millis(); // Remember last run time.
   }
@@ -39,17 +38,24 @@ void CheckDoorStateForAlarm()
 {
   if (digitalRead(PIN_DOOR_SWITCH) == 0)
   {
-    door_counter++;
+    door_counter++; // Doorswitch is zero --> Debouncing
   }
-  else
+  else // Door is closed
   {
     door_counter = -1;
+    door_state = 1;
   }
 
-  if (door_counter >= 200) // Door must opened for some time
+  if (door_counter >= 40000) // Door must opened for some time --> Debouncing
   {
-    door_state = 0;
-    watchdog = 0;
+    door_state = 0;    // Door open
+    watchdog = 0;      // Real Alarm
+    door_counter = -1; // Door Counter Reset
+
+    Serial.println("Door open!");
+
+    CheckAlarm_SendAlarmLoraPackage();
+    resetToDefaultValues();
   }
 }
 
@@ -59,24 +65,21 @@ void setup()
   Serial.println("Starting...");
   Serial.println("Sketch: " VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH "." BUILD_COMMIT "-" BUILD_BRANCH);
   Serial.println("Builddate: " BUILD_DATE " " BUILD_TIME);
+
+  Setup_Pins();
+  door_state = digitalRead(PIN_DOOR_SWITCH);
+  Blink_Info_LED(50, 15); // LED blink (The LED can only be used once at the beginning due to SPI PIN/collision)
+
   PrintResetReason();
   LoRaWANVersion();
   delay(3000);
   PowerDownSetupWatchdog();
-  Setup_Pins();
-  Blink_Info_LED(50, 15); // LED blink (The LED can only be used once at the beginning due to SPI PIN/collision)
   LoRaWANSetup();
   disableDeepSleep(); // DeepSleep Disable
 }
 
-void loop()
+void loop() // Infinity Loop
 {
   CheckDoorStateForAlarm();
-
-  if (!os_queryTimeCriticalJobs(5000))
-  {
-    CheckDoorStateAndSendLoraPackage();
-  }
-
   LoRaWANDo();
 }
