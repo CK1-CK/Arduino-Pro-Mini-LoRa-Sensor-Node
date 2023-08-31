521921dcd14735aeeb480ebe14de25bc8409e567
CK1 <kreissc1@gmail.com>
2023-08-31 19:43:18 +0200

    Intervall Change

--- src/global.cpp
+++ src/global.cpp
@@ -5,4 +5,4 @@ volatile uint8_t doorState = 0;                // DOOR-Status open/closed
 volatile uint8_t watchdog = 1;                  // Watchdog, Differentiation between watchdog and real alarm.   1=WatchDogSignal 0=Real DoorAlarm
 volatile uint8_t AlarmModeEnabled = 0;         // Alarmmode 1=Enabled
 volatile unsigned long oldTime = 0;
-volatile unsigned long minSendIntervall = 20*60*1000; // Intervall to Send Alarm Pakages in ms  1200000 = 20min | 60000=1min
+volatile unsigned long minSendIntervall = 180000; // Intervall to Send Alarm Pakages in ms 60000=1min
